8c6b3090841872fbf587ca65874e7dcc6c4e2549
paulantoinerichard <114244040+paulantoinerichard@users.noreply.github.com>
2023-11-02 15:26:51 +0100

    multi2.py

--- BIG DATA/Charbonnel/multi2.py
+++ BIG DATA/Charbonnel/multi2.py
@@ -3,6 +3,7 @@ import os, sys, glob, time, importlib, logging, multiprocessing, shutil
 # Lancer avec python multi2.py .\jobs 3
 
 def run_job(job):
+    # Dans cette fonction, on va créer le app.log, tester si les jobs fonctionnent et ajouter toutes les informations dans le app.log
     logging.basicConfig(level=logging.DEBUG, filename="app.log", filemode="a", format='%(asctime)s - %(levelname)s - %(message)s')
     index, module_name = job  # Découpez le tuple en index et nom_de_module
     start_time = time.time()
@@ -21,7 +22,7 @@ def run_job(job):
 
     with open(f"{module_name}.result", "w") as fichier:
         fichier.write(str(resultat))
-    logging.info(f'{module_name} executed by process {process_name}')
+    logging.info(f'{module_name} executed by process {process_name}')            # Pour savoir quel processeur a effectué quel tâche
     logging.info(f"File_name = {module_name}.py,\t Début : {start_time},\t Fin : {finish_time}")
     logging.info(f"Temps total : {finish_time - start_time}")
     logging.info(f"Statut : {status}")
@@ -31,24 +32,24 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Utilisation : python multi2.py <chemin vers jobs> <nombre de processus>")
         sys.exit(1)
-    begin=time.time()
+    begin=time.time()                                                        # Début du compte du temps total
 
-    script_path = sys.argv[1]
+    script_path = sys.argv[1]                                                # On récupère les infos de chemin et de n
     nb_processes = int(sys.argv[2])
 
-    os.chdir(script_path)
+    os.chdir(script_path)                                                    # On fait une liste de tous les noms des fichiers jobs
     jobs_files = glob.glob('*.py')
     jobs = [module[:-3] for module in jobs_files]
     job_list = list(enumerate(jobs))
 
-    with multiprocessing.Pool(processes=nb_processes) as pool:
+    with multiprocessing.Pool(processes=nb_processes) as pool:               # On fait du multiprocessing
         pool.map(run_job, job_list)
 
-    fichier_log_source = 'C:\\Users\\rpaul\\Desktop\\MULTTT\\jobs\\app.log'
+    fichier_log_source = 'C:\\Users\\rpaul\\Desktop\\MULTTT\\jobs\\app.log'  # On déplace dans le dossier supérieur le app.log pour le modifier
     dossier_de_destination = 'C:\\Users\\rpaul\\Desktop\\MULTTT'
 
     shutil.move(fichier_log_source, dossier_de_destination)
     logging.basicConfig(level=logging.DEBUG, filename=os.path.join(dossier_de_destination, 'app.log'), filemode="a", format='%(asctime)s - %(levelname)s - %(message)s')
 
-    logging.info(f"Nombre de processeurs : {nb_processes}, temps total : {time.time()-begin}")
+    logging.info(f"Nombre de processeurs : {nb_processes}, temps total : {time.time()-begin}")     # On ajoute au log le nombre de processeurs ainsi que le temps total
 
